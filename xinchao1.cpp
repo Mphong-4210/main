8ef07fe1362ea2d63818a0ddaec531c9b7a3a423
Manh Phong Pham <phammanhphong2010@gmail.com>
2025-10-07 14:08:42 +0700

    Update xinchao1.cpp

---
+++ xinchao1.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fl float
+
+void solve(){
+    int n;
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        string s;
+        ll chu=0;
+        cin>>s;
+        for(ll i=0; i<s.size(); i++){
+            if(isalpha(s[i])){
+                chu+=1;
+            }
+        }
+
+        if(chu!=s.size()){
+            cout<<"NO"<<endl;
+        }
+        else{
+            string tar="hello";
+            int j=0, dem=0;
+            for(int i=0; i<s.size(); i++){
+                if(s[i]==tar[j]){
+                    j+=1;
+                    if(j==6){
+                        break; 
+                    }
+                }
+            }
+            if(j==5){
+                cout<<"YES"<<endl;
+            }
+            else{
+                cout<<"NO"<<endl;
+            }
+        }
+    }
+}
+
+int main(){
+    solve();
+}
