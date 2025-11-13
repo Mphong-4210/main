f318998c1d2d6035068cf4bd314d8bdb1af2d837
Manh Phong Pham <phammanhphong2010@gmail.com>
2025-11-13 20:30:21 +0700

    Update dayconnam.cpp

---
+++ dayconnam.cpp
@@ -0,0 +1,34 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define db double
+#define fl float
+#define el "\n"
+#define ull unsigned long long
+
+const int maxn=10e6+42;
+const int N=1e3;
+int y[maxn], a[N][N], x[maxn];
+
+
+void solve(){
+    int n;
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        cin>>a[i];
+        for(int j=1; j<=a[i]; j++){
+            cin>>b[j];
+        }
+        
+    }
+}
+
+int main(){
+       freopen("DAYCONAMNN.INP", "r", stdin);
+       freopen("DAYCONAMNN.OUT", "w", stdout);
+       ios::sync_with_stdio(false);
+       cin.tie(nullptr);
+       solve();
+}
