577d11235e9d577d3ce13db10350a2f953f8fba1
Manh Phong Pham <phammanhphong2010@gmail.com>
2025-11-13 20:31:18 +0700

    Update nguyentocungnhau.cpp

---
+++ nguyentocungnhau.cpp
@@ -0,0 +1,28 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define db double
+#define fl float
+#define el "\n"
+#define ull unsigned long long
+#define fi first
+#define se second
+
+const int maxn=10e6+42;
+const int N=1e3;
+int y[maxn], a[N][N], x[maxn];
+
+
+void solve(){
+ 
+}
+
+int main(){
+       //freopen(".INP", "r", stdin);
+       //freopen(".OUT", "w", stdout);
+       ios::sync_with_stdio(false);
+       cin.tie(nullptr);
+       solve();
+}
