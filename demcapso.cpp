885220bb9c4316e0f597affa28e59cb2eab46dea
Manh Phong Pham <phammanhphong2010@gmail.com>
2025-11-13 20:30:34 +0700

    Update demcapso.cpp

---
+++ demcapso.cpp
@@ -0,0 +1,34 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define db double
+#define fl float
+#define el "\n"
+#define ull unsigned long long
+#define fi first
+#define se second
+
+const int maxn=10e6+42;
+const int N=1e3;
+int a[maxn];
+
+void solve(){
+    int T;
+    cin>>T;
+    while(T--){
+        int n;
+        for(int i=1; i<=n; i++){
+            cin>>a[i];
+        }
+    }
+}
+
+int main(){
+       freopen("DEMCAPSO.INP", "r", stdin);
+       freopen("DEMCAPSO.OUT", "w", stdout);
+       ios::sync_with_stdio(false);
+       cin.tie(nullptr);
+       solve();
+}
