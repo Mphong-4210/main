ca6f517c942545d22fd3f0a189acc06cc82a3ea2
Manh Phong Pham <phammanhphong2010@gmail.com>
2026-02-18 23:16:30 +0700

    Create demphanphoi2.cpp

---
+++ demphanphoi2.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define db double
+#define fl float
+#define el "\n"
+#define ull unsigned long long
+
+const int maxn=10e6+42;
+const int N=1e3;
+int y[maxn], a[N][N], x[maxn], dem[maxn];
+
+
+void solve(){
+    int n;
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        cin>>y[i];
+        dem[y[i]]++;
+    }
+    sort(y+1, y+n+1);
+    for(int i=1; i<=n; i++){
+        if(y[i]!=y[i+1]){
+            cout<<y[i]<<' ';
+            cout<<dem[y[i]]<<el;
+        }
+        else{
+            continue;
+        }
+    }
+
+
+}
+
+int main(){
+       //freopen(".INP", "r", stdin);
+       //freopen(".OUT", "w", stdout);
+       ios::sync_with_stdio(false);
+       cin.tie(nullptr);
+       solve();
+}
