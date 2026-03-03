0496c4a12d5096306297942caf3963b400aafc10
Manh Phong Pham <phammanhphong2010@gmail.com>
2026-03-03 20:13:37 +0700

    Create sohoanhao.cpp

---
+++ sohoanhao.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fl float
+#define el "\n"
+#define ull unsigned long long
+
+const int maxn=10e6+42;
+int a[maxn];
+
+ll tud(ll a){
+    ll dem=0;
+    for(int i=1; i*i<=a; i++){
+        if(a%i==0){
+            dem+=i;
+            if (i != a / i) dem += a / i;
+        }
+
+    }
+    return dem;
+}
+void solve(){
+    ll n, dem=0;
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        cin>>a[i];
+        if(a[i]*2<=tud(a[i])){
+            dem+=1;
+        }
+    }
+    cout<<dem;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+}
