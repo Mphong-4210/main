1ec9bac2b0fdc7344fdedc1ba657bd4161d02eb5
Manh Phong Pham <phammanhphong2010@gmail.com>
2026-03-16 22:57:43 +0700

    Create trangtriden.cpp

---
+++ trangtriden.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+#define el '\n'
+#define ll long long
+#define ull unsigned long long
+#define fumo ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define pb push_back
+#define str string
+#define sz size()
+#define ldb long double
+#define db double
+#define reimu(v) v.begin(), v.end()
+#define ret return
+#define skip cin.ignore()
+#define fi first
+#define se second
+#define int long long
+using namespace std;
+//ᗜˬᗜ
+const int N=1e6+7;
+const ll mod=1e9+7,inf=1e18;
+int t;
+ll n,x;
+
+void sol()
+{
+    cin>>n>>x;
+    cout<<(n+1)/2*x;
+}
+
+void ffopen(const str &name)
+{
+    if(!name.empty()){
+        freopen((name+".inp").c_str(), "r", stdin);
+        freopen((name+".out").c_str(), "w", stdout);
+    }
+}
+
+signed main()
+{
+    fumo
+    ffopen("ttd");
+    t=1;
+    //cin>>t;
+    while(t--) sol();
+    ret 0;
+}
