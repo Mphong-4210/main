dad3b0a8bf6034cac7cc378fc13931a90d024483
Manh Phong Pham <phammanhphong2010@gmail.com>
2026-03-22 15:37:20 +0700

    Add files via upload

---
+++ luyentap.cpp
@@ -0,0 +1,79 @@
+
+#pragma GCC optimize("O3")
+#pragma GCC target("avx2")
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define db double
+#define fl float
+#define str string
+#define ldb long double
+
+#define vll vector<ll>
+#define pll pair<ll,ll>
+#define mll map<ll,ll>
+#define dq deque<ll>
+
+#define ret return
+#define el '\n'
+#define sz size()
+#define fi first
+#define se second
+#define pb push_back
+#define pob pop_back
+// good luck coding:)
+
+const ll INF = 1e18, NINF = -1e18;
+const int MAXN=1e6+42, N=1e3, MOD=1000000007;
+const int dx[4] = {1,-1,0,0}, dy[4] = {0,0,1,-1};
+ll a[MAXN], dem[MAXN], y[N][N], pre[MAXN];
+
+void manhphong(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+void ffopen(string name){
+    string infile=name + ".inp";
+    string outfile=name + ".out";
+    if(FILE* f = fopen(infile.c_str(), "r")){
+        fclose(f);
+        freopen(infile.c_str(), "r", stdin);
+        freopen(outfile.c_str(), "w", stdout);
+    }
+}
+
+void solve(){
+    ll n;
+    cin>>n;
+    ll ans = 0;
+    for(ll i=1; i<=n; ){
+        ll k=n/i;
+        ll j=n/k;   
+        ans+=k*(j-i+1);
+        i=j+1;
+    }
+    cout<<ans;
+}
+
+int main(){
+    //ffopen("TINHTONG");
+    manhphong();
+    solve();
+}
+
+/*
+   ____  _                             
+  |  _ \| |__   ___  _ __   __ _ 
+  | |_) | '_ \ / _ \| '_ \ / _` |
+  |  __/| | | | (_) | | | | (_| |
+  |_|   |_| |_|\___/|_| |_|\__, |
+                           |___/       
+               /\_/\
+              ( o.o )
+               > ^ <
+               /   \
+*/
