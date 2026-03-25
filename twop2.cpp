de5146c788c17a52c1b2053e2be07dd22bd1d558
Manh Phong Pham <phammanhphong2010@gmail.com>
2026-03-25 22:24:06 +0700

    Create twop2.cpp

---
+++ twop2.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define db double
+#define fl float
+
+#define el "\n"
+#define fi first
+#define se second
+#define pb push_back
+
+const int maxn=10e6+42;
+const int N=1e3;
+int a[maxn], y[N][N], dem[maxn], b[maxn];
+
+
+void solve(){
+    ll n, m, res=0;
+    cin>>n>>m;
+    for(int i=1; i<=n; i++){cin>>a[i];}
+    for(int i=1; i<=m; i++){cin>>b[i];}
+    int i=1, j=1;
+    while(i<=n && j<=m){
+        if(a[i]<b[j]) i++;
+        else if(a[i] > b[j]) j++;
+        else {
+            long long val = a[i];
+            long long cntA = 0, cntB = 0;
+
+            while(i <= n && a[i] == val){
+                cntA++;
+                i++;
+            }
+            while(j <= m && b[j] == val){
+                cntB++;
+                j++;
+            }
+
+            res += cntA * cntB;
+        }
+    }
+    cout<<res;
+}
+
+int main(){
+       //freopen(".INP", "r", stdin);
+       //freopen(".OUT", "w", stdout);
+       ios::sync_with_stdio(false);
+       cin.tie(nullptr);
+       solve();
+}
