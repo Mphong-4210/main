411a39c50fe7d551ac7fc1d6539b43c6a981bb4b
Manh Phong Pham <phammanhphong2010@gmail.com>
2026-04-12 10:14:56 +0700

    Add main function to compute sum using priority queue
    Implement a priority queue to calculate the sum of inputs.

---
+++ noiday2.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+        priority_queue<long long> pq;
+        for (int i = 0; i < n; i++) {
+            long long x;
+            cin >> x;
+            if(n==1){
+                cout<<x;
+            return 0;
+        }
+            pq.push(x);
+        }
+        
+
+        long long total = 0;
+        while (pq.size() > 1) {
+            long long a = pq.top(); pq.pop();
+            long long b = pq.top(); pq.pop();
+            total += a + b;
+            pq.push(a + b);
+        }
+    
+        cout << total % long(1e9+7) << endl;
+        return 0;
+}
