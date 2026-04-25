d8b0662e9a96460fab5b3d79ba4fdae3c2f3fc27
Manh Phong Pham <phammanhphong2010@gmail.com>
2026-04-25 10:53:41 +0700

    Add files via upload

---
+++ chinhphuong.cpp
@@ -0,0 +1,80 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define db double
+#define fl float
+#define str string
+#define ldb long double
+#define vll vector<ll>
+#define pll pair<ll,ll>
+#define mll map<ll,ll>
+#define dq deque<ll>
+#define bend(x) (x).begin(), (x).end()
+#define ret return
+#define el '\n'
+#define si size()
+#define fi first
+#define se second
+#define pb push_back
+#define pob pop_back
+#define FOR(i,a,b) for(int i=(a); i<=(b); i++)
+#define FORN(i,a,b) for(int i=(a); i>=(b); i--)
+// Phong
+
+const ll INF = 1e18, NINF = -1e18;
+const int MAXN=1e6+42, N=1e3, MOD=2021;
+const int dx[4] = {1,-1,0,0}, dy[4] = {0,0,1,-1};
+ll a[MAXN], dem[MAXN], y[N][N], pre[MAXN];
+
+void manhphong(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+void ffopen(string name){
+    string infile=name + ".inp";
+    string outfile=name + ".out";
+    if(FILE* f = fopen(infile.c_str(), "r")){
+        fclose(f);
+        freopen(infile.c_str(), "r", stdin);
+        freopen(outfile.c_str(), "w", stdout);
+    }
+}
+
+ll mod = 2021;
+
+void solve(){
+    ll n;
+    cin>>n;
+    ll a=n, b=n+1, c=2*n+1;
+    a%=mod; b%=mod; c%=mod;
+
+    if(a%2==0){a/=2;}
+    else{b/=2;}
+
+    if(a%3==0){a/=3;}
+    else if(b%3==0){b/=3;}
+    else{c/=3;}
+    
+    ll res = ((a*b)%mod*c)%mod;
+    cout<<res;
+}
+
+int main(){
+    //ffopen("filename");
+    manhphong();
+    solve();
+}
+
+/*
+   ____  _                             
+  |  _ \| |__   ___  _ __   __ _ 
+  | |_) | '_ \ / _ \| '_ \ / _` |
+  |  __/| | | | (_) | | | | (_| |
+  |_|   |_| |_|\___/|_| |_|\__, |
+                           |___/                           
+*/
