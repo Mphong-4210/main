fee19b5ac6e672f50dd25a5b635b710963321556
Manh Phong Pham <phammanhphong2010@gmail.com>
2026-04-29 15:13:52 +0700

    Create game.cpp

---
+++ game.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define db double
+#define fl float
+#define str string
+#define ldb long double
+#define vll vector<ll>
+#define pll pair<ll,ll>
+#define mll map<ll,ll>
+#define dq deque<ll>
+#define bend(x) (x).begin(), (x).end()
+#define ret return
+#define el '\n'
+#define si size()
+#define fi first
+#define se second
+#define pb push_back
+#define pob pop_back
+#define FOR(i,a,b) for(int i=(a); i<=(b); i++)
+#define FORN(i,a,b) for(int i=(a); i>=(b); i--)
+// Phong
+
+const ll INF = 1e18, NINF = -1e18;
+const int MAXN=1e6+42, N=1e3, MOD=1000000007;
+const int dx[4] = {1,-1,0,0}, dy[4] = {0,0,1,-1};
+ll a[MAXN], dem[MAXN], y[N][N], pre[MAXN];
+
+void manhphong(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+void ffopen(string name){
+    string infile=name + ".inp";
+    string outfile=name + ".out";
+    if(FILE* f = fopen(infile.c_str(), "r")){
+        fclose(f);
+        freopen(infile.c_str(), "r", stdin);
+        freopen(outfile.c_str(), "w", stdout);
+    }
+}
+
+void solve(){
+    ll a, b;
+    cin>>a>>b;
+    if(a==b){
+        cout<<a+a;
+    }
+    else if(a>b){
+        cout<<a+a-1;
+    }
+    else{
+        cout<<b+b-1;
+    }
+}
+
+int main(){
+    ffopen("game");
+    manhphong();
+    solve();
+}
+
+/*
+   ____  _                             
+  |  _ \| |__   ___  _ __   __ _ 
+  | |_) | '_ \ / _ \| '_ \ / _` |
+  |  __/| | | | (_) | | | | (_| |
+  |_|   |_| |_|\___/|_| |_|\__, |
+                           |___/                           
+*/
