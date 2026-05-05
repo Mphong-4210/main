337e48dfb50bdd0b411b5be7ef628b1669c0535d
Manh Phong Pham <phammanhphong2010@gmail.com>
2026-05-05 10:25:29 +0700

    Create backtrack1.cpp

---
+++ backtrack1.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+void backtrack(int x, string s, int n) {
+    if (x == n) {
+        cout << s << endl;
+        return;
+    }
+    s[x] = '0';
+    backtrack(x + 1, s, n); 
+    s[x] = '1';
+    backtrack(x + 1, s, n); 
+}
+int main() {
+    int n;
+    cin >> n;
+    if (n > 0) {
+        string s(n, '0');
+        backtrack(0, s, n); 
+    } else {
+        cout << endl; 
+    }
+    return 0;
+}
