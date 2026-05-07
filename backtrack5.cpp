1b9d7d44a6506ceb3a499ffc072d9092370e9b1a
Manh Phong Pham <phammanhphong2010@gmail.com>
2026-05-07 16:58:02 +0700

    Create backtrack5.cpp

---
+++ backtrack5.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long dem=0;
+int n, k;
+vector<int> a; 
+void backtrack(int x, int y) {
+    if (x == k) {
+        cout << "("; 
+        for(int i = 0; i < a.size(); ++i) {
+            cout << a[i];
+            if (i != a.size() - 1) cout << ",";
+        }
+        cout << ")";
+        cout << endl;
+        dem++;
+        return;
+    }
+    for (int i = y; i <= n - (k - x) + 1; ++i) {
+        a.push_back(i);
+        backtrack(x + 1, i + 1);
+        a.pop_back();  
+    }
+}
+
+int main() {
+    cin >> n;
+    cin >> k;
+    backtrack(0, 1);
+    cout << dem;
+    return 0;
+}
