05a09de0f9dbaa2a6da136047842205bc971f780
Manh Phong Pham <phammanhphong2010@gmail.com>
2026-06-01 22:31:27 +0700

    Create muasam1.cpp

---
+++ muasam1.cpp
@@ -0,0 +1,55 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2")
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define db double
+#define fl float
+#define str string
+#define ldb long double
+#define vll vector<ll>
+#define pll pair<ll,ll>
+#define mll map<ll,ll>
+#define dq deque<ll>
+#define bend(x) (x).begin(), (x).end()
+#define ret return
+
+ll n, q;
+vll a, dp;
+
+void solve() {
+    cin >> n >> q;
+    a.resize(n + 1);
+
+    ll sum = 0;
+    for (ll i = 1; i <= n; i++) {
+        cin >> a[i];
+        sum += a[i];
+    }
+
+    sort(a.begin() + 1, a.end());
+
+    dp.assign(n + 1, 0);
+
+    for (ll i = 1; i <= n; i++) {
+        ll disc = a[i] * q / 100;
+
+        dp[i] = dp[i - 1] + disc;
+
+        if (i >= 3)
+            dp[i] = max(dp[i], dp[i - 3] + a[i - 2]);
+    }
+
+    cout << sum - dp[n];
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+    return 0;
+}
