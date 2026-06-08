d40869d9ab2cedaafdf8f95475b7a46f2bd0bb0a
Manh Phong Pham <phammanhphong2010@gmail.com>
2026-06-08 19:00:10 +0700

    Create v11415_bobasotamgiac.cpp

---
+++ v11415_bobasotamgiac.cpp
@@ -0,0 +1,78 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2")
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define db double
+#define fl float
+#define str string
+#define ldb long double
+#define vll vector<ll>
+#define pll pair<ll,ll>
+#define mll map<ll,ll>
+#define dq deque<ll>
+#define ret return
+#define el '\n'
+#define si size()
+#define fi first
+#define se second
+#define pb push_back
+#define pob pop_back
+#define bend(x) (x).begin(), (x).end()
+#define FOR(i,a,b) for(int i=(a); i<=(b); i++)
+#define FORN(i,a,b) for(int i=(a); i>=(b); i--)
+// Phong
+ 
+const ll INF = 1e18, NINF = -1e18;
+const int MAXN=1e6+42, N=1e3, MOD=1000000007;
+const int dx[4] = {1,-1,0,0}, dy[4] = {0,0,1,-1};
+ll a[MAXN], dem[MAXN], y[N][N], pre[MAXN];
+ 
+void manhphong(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+ 
+void ffopen(string name){
+    string infile=name + ".inp";
+    string outfile=name + ".out";
+    if(FILE* f = fopen(infile.c_str(), "r")){
+        fclose(f);
+        freopen(infile.c_str(), "r", stdin);
+        freopen(outfile.c_str(), "w", stdout);
+    }
+}
+ 
+void solve(){
+    ll n;
+    cin>>n;
+    ll ans = 0;
+    if (n % 2 == 0) {
+        ans = n * (n - 2) * (2 * n - 5) / 24;
+    }
+    else{
+        ans = (n - 1) * (n - 3) * (2 * n - 1) / 24;
+    }
+    ll MOD = 1e9;
+    cout << ans % MOD << endl; 
+}
+ 
+int main(){
+    ffopen("Bai1");
+    manhphong();
+    solve();
+    return 0;
+}
+ 
+/*
+   ____  _                             
+  |  _ \| |__   ___  _ __   __ _ 
+  | |_) | '_ \ / _ \| '_ \ / _` |
+  |  __/| | | | (_) | | | | (_| |
+  |_|   |_| |_|\___/|_| |_|\__, |
+                           |___/                           
+*/
