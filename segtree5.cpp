7f1ecdbac0587d5d4e0a508d577d8fb271d73de4
Manh Phong Pham <phammanhphong2010@gmail.com>
2026-07-15 21:39:05 +0700

    Create segtree5.cpp

---
+++ segtree5.cpp
@@ -0,0 +1,90 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2")
+
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double db;
+typedef float fl;
+typedef long double ldb;
+typedef unsigned long long ull;
+typedef string str;
+#define vll vector<ll>
+#define pll pair<ll,ll>
+#define mll map<ll,ll>
+#define dq deque<ll>
+#define ret return
+#define si size()
+#define el '\n'
+#define fi first
+#define se second
+#define pb push_back
+#define pf push_front
+#define pob pop_back
+#define pof pop_front
+#define bend(x) (x).begin(), (x).end()
+#define FOR(i,a,b) for(int i=(a); i<=(b); i++)
+#define FORN(i,a,b) for(int i=(a); i>=(b); i--)
+// Phong
+
+const ll INF = 4e18, NINF = -4e18;
+const int MAXN=1e6+42, MAXM=1e3, MOD=1000000007;
+const int dx[4]={1,-1,0,0}, dy[4]={0,0,1,-1};
+ll a[MAXN], b[MAXN];
+
+void manhphong(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+void ffopen(string name){
+    string infile=name + ".inp";
+    string outfile=name + ".out";
+    if(FILE* f = fopen(infile.c_str(), "r")){
+        fclose(f);
+        freopen(infile.c_str(), "r", stdin);
+        freopen(outfile.c_str(), "w", stdout);
+    }
+}
+
+ll kc(ll xi, ll xj, ll yi, ll yj){
+    return min(abs(xi - xj), abs(yi - yj));
+}
+
+void solve(){
+    ll n, k;
+    cin>>n>>k;
+    ll a[n][2];
+    vll b;
+    for(ll i=0; i<=n; i++){
+        cin>>a[i][1]>>a[i][2];
+    }
+    for(ll i=0; i<n; i++){
+        for(ll j=i+1; j<n; j++){
+            b.pb(kc(a[i][1], a[j][1], a[i][2], a[j][2]));
+        }
+    }
+    sort(b.begin(), b.end());
+    cout<<b[k-1];
+}
+
+signed main(){
+    //ffopen("filename");
+
+    manhphong();
+
+    solve();
+    
+    ret 0;
+}
+
+/*
+   ____  _                             
+  |  _ \| |__   ___  _ __   __ _ 
+  | |_) | '_ \ / _ \| '_ \ / _` |
+  |  __/| | | | (_) | | | | (_| |
+  |_|   |_| |_|\___/|_| |_|\__, |
+                           |___/                           
+*/
