fce465c2141a6fc9a9df5d4c703a2474254b42d0
Manh Phong Pham <phammanhphong2010@gmail.com>
2026-08-07 19:38:17 +0700

    Create dt26_17.cpp

---
+++ dt26_17.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long a, b;
+    cin >> a >> b;
+
+    long long sum = a + 2 * b;
+    if (sum & 1) {
+        cout << 0;
+        return 0;
+    }
+
+    long long t = sum / 2;
+
+    for (long long y = 0; y <= b; y++) {
+        long long x = t - 2 * y;
+        if (0 <= x && x <= a) {
+            cout << 1;
+            return 0;
+        }
+    }
+
+    cout << 0;
+    return 0;
+}
