c220db03ef00570040e81250af8ac8754486891f
Manh Phong Pham <phammanhphong2010@gmail.com>
2026-09-10 19:27:05 +0700

    Add initial implementation of hsg2025_b_1.cpp

---
+++ hsg2025_b_1.cpp
@@ -0,0 +1,116 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2")
+
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double db;
+typedef float fl;
+typedef long double ldb;
+typedef unsigned long long ull;
+typedef string str;
+#define vll vector<ll>
+#define pll pair<ll,ll>
+#define mll map<ll,ll>
+#define dq deque<ll>
+#define ret return
+#define si size()
+#define el '\n'
+#define fi first
+#define se second
+#define pb push_back
+#define pf push_front
+#define pob pop_back
+#define pof pop_front
+#define bend(x) (x).begin(), (x).end()
+#define FOR(i,a,b) for(int i=(a); i<=(b); i++)
+#define FORN(i,a,b) for(int i=(a); i>=(b); i--)
+// Phong
+
+const ll INF = 4e18, NINF = -4e18;
+const int MAXN=1e6+42, MAXM=1e3, MOD=1000000007;
+const int dx[4]={1,-1,0,0}, dy[4]={0,0,1,-1};
+ll a[MAXN], dem[MAXN], y[MAXM][MAXM], pre[MAXN];
+
+void manhphong(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+void ffopen(string name){
+    string infile=name + ".inp";
+    string outfile=name + ".out";
+    if(FILE* f = fopen(infile.c_str(), "r")){
+        fclose(f);
+        freopen(infile.c_str(), "r", stdin);
+        freopen(outfile.c_str(), "w", stdout);
+    }
+}
+
+void solve(){
+    string s;
+    cin>>s;
+    
+    for(int i=0; i<=1; i++){
+        if(isupper(s[i])){
+            continue;
+        }
+        else{
+            cout<<i+1;
+            return;
+        }
+    }
+    if(s[2]!='-'){
+        cout<<2;
+        return;
+    }
+    for(int i=3; i<=5; i++){
+        if(isdigit(s[i])){
+            continue;
+        }
+        else{
+            cout<<i+1;
+            return;
+        }
+    }
+    if(s[6]!='.'){
+        cout<<7;
+        return;
+    }
+    for(int i=7; i<=8; i++){
+        if(isdigit(s[i])){
+            continue;
+        }
+        else{
+            cout<<i+1;
+            return;
+        }
+    }
+    if(s.size()==9){
+        cout<<0;
+    }
+    else{
+        cout<<10;
+    }
+}
+
+signed main(){
+    //ffopen("filename");
+
+    manhphong();
+
+    solve();
+    
+    ret 0;
+}
+
+/*
+   ____  _                             
+  |  _ \| |__   ___  _ __   __ _ 
+  | |_) | '_ \ / _ \| '_ \ / _` |
+  |  __/| | | | (_) | | | | (_| |
+  |_|   |_| |_|\___/|_| |_|\__, |
+                           |___/                           
+*/
