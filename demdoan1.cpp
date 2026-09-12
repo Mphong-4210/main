a8555e537d617cc1e69390ccd788c65782ac5c2f
Manh Phong Pham <phammanhphong2010@gmail.com>
2026-09-12 23:21:18 +0700

    Create demdoan1.cpp

---
+++ demdoan1.cpp
@@ -0,0 +1,106 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2")
+
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double db;
+typedef float fl;
+typedef long double ldb;
+typedef unsigned long long ull;
+typedef string str;
+#define vll vector<ll>
+#define pll pair<ll,ll>
+#define mll map<ll,ll>
+#define dq deque<ll>
+#define ret return
+#define si size()
+#define el '\n'
+#define fi first
+#define se second
+#define pb push_back
+#define pf push_front
+#define pob pop_back
+#define pof pop_front
+#define bend(x) (x).begin(), (x).end()
+#define FOR(i,a,b) for(int i=(a); i<=(b); i++)
+#define FORN(i,a,b) for(int i=(a); i>=(b); i--)
+// Phong
+
+const ll INF = 4e18, NINF = -4e18;
+const int MAXN=1e6+42, MAXM=1e3, MOD=1000000007;
+const int dx[4]={1,-1,0,0}, dy[4]={0,0,1,-1};
+ll a[MAXN], dem[MAXN], y[MAXM][MAXM], pre[MAXN];
+
+void manhphong(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+void ffopen(string name){
+    string infile=name + ".inp";
+    string outfile=name + ".out";
+    if(FILE* f = fopen(infile.c_str(), "r")){
+        fclose(f);
+        freopen(infile.c_str(), "r", stdin);
+        freopen(outfile.c_str(), "w", stdout);
+    }
+}
+
+void solve(){
+    int n;
+    cin>>n;
+
+    for(int i=1;i<=n;i++) cin>>a[i];
+
+    ll ans=0;
+
+    for(int l=1;l<=n;l++){
+        for(int r=l;r<=n;r++){
+
+            int dem[4]={0,0,0,0};
+
+            for(int k=l;k<=r;k++){
+                dem[a[k]]++;
+            }
+
+            int cnt=0;
+            int sl=-1;
+            bool ok=true;
+
+            for(int x=0;x<=3;x++){
+                if(dem[x]>0){
+                    cnt++;
+
+                    if(sl==-1) sl=dem[x];
+                    else if(dem[x]!=sl) ok=false;
+                }
+            }
+
+            if(cnt==3 && ok) ans++;
+        }
+    }
+
+    cout<<ans;
+}
+
+signed main(){
+    //ffopen("filename");
+
+    manhphong();
+
+    solve();
+    
+    ret 0;
+}
+
+/*
+   ____  _                             
+  |  _ \| |__   ___  _ __   __ _ 
+  | |_) | '_ \ / _ \| '_ \ / _` |
+  |  __/| | | | (_) | | | | (_| |
+  |_|   |_| |_|\___/|_| |_|\__, |
+                           |___/                           
+*/
